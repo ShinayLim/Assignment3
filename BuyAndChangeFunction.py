c95ffe8202a55c060f99fc6a935b0911deb9e011
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-02 01:50:52 -0700

    second commit

--- BuyAndChangeFunction.py
+++ BuyAndChangeFunction.py
@@ -3,3 +3,14 @@ name = str (input("Good day! Please enter your nickname: "))
 def greet(name):
     print ("Good day "+ str(name), "! Please enjoy your shopping.")
 greet(name)
+
+def getMoney():
+    money = input("How much money do you have on hand?: ")
+    return money
+
+def askForApplePrice():
+    priceOfApple = input("What is the current cost of an apple?: ")
+    return  priceOfApple
+
+money = getMoney()
+priceOfApple = askForApplePrice()
