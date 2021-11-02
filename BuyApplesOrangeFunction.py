a31701499a27061fad296bdcf912c6a349617067
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-02 11:08:25 -0700

    third commit

--- BuyApplesOrangeFunction.py
+++ BuyApplesOrangeFunction.py
@@ -9,3 +9,16 @@ def greet(name):
     print ("Good day "+ str(name), "! Please enjoy your shopping.")
 greet(name)
 
+#this will ask the customer the number of
+#   apples and oranges they want to have
+#   and its total cost
+
+applePrice = 20
+orangePrice = 25
+
+def askForNumberOfApple():
+    applesYouWantToBuy = input("How many apple/s you want to add in your cart?: ")
+    costOfApple = int(applesYouWantToBuy) * applePrice
+    return costOfApple
+
+costOfApple = askForNumberOfApple()
