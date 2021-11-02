d52945dc9ffed45d23066592e020b341c9f9e9e6
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-02 01:39:47 -0700

    sixth commit

--- NameAgeAddFunction.py
+++ NameAgeAddFunction.py
@@ -1,9 +1,10 @@
 name = str (input("Welcome to our shop, what is your nickname? "))
-
+# this will greet the person's name
 def greet(name):
     print("Hello, "+name+". Good Morning!")
 greet(name)
 
+# this will ask the person if the system is allowed to have their personal details
 def askYesNoQuestion(question):
   YesNoAnswer = input(question).upper()
   if YesNoAnswer == "YES" or YesNoAnswer == "NO":
@@ -16,7 +17,8 @@ if answer == "YES":
   print("We highly appreciate that. We may now proceed.")
 elif answer == "NO":
     print("We understand your privacy concerns.Thank you.")
-
+    
+#gathering of the personal details...
 def getName():
     nameFunc = input("Enter your full name: ")
     return nameFunc
@@ -30,6 +32,7 @@ def getAddress():
     return addFunc
 
 def displayOutput(nameF, ageF, addF):
+  print ("Please check if your information below is right...")
   print (f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addF}.")
 
 name = getName()
