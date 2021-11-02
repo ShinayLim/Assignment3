c3057bee424a8f134422655b8794aee46707ada9
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-02 02:23:05 -0700

    second commit

--- BuyAndChangeFunction.py
+++ BuyAndChangeFunction.py
@@ -4,13 +4,29 @@ def greet(name):
     print ("Good day "+ str(name), "! Please enjoy your shopping.")
 greet(name)
 
-def getMoney():
+def askForMoneyYouHave():
     money = input("How much money do you have on hand?: ")
+    money = int(money)
     return money
 
-def askForApplePrice():
+def askForPriceOfApple():
     priceOfApple = input("What is the current cost of an apple?: ")
-    return  priceOfApple
+    priceOfApple = int(priceOfApple)
+    return priceOfApple
 
-money = getMoney()
-priceOfApple = askForApplePrice()
+def numberOfApplesYouCanBuy():
+    numOfApple = customersMoney//applePrice
+    return numOfApple
+
+def yourChange():
+    change = customersMoney%applePrice
+    return change
+
+def displayOutput():
+    print(f"You can buy {numOfApple} apples and your change is {change} pesos.")
+
+customersMoney = askForMoneyYouHave()
+applePrice = askForPriceOfApple()
+numOfApple = numberOfApplesYouCanBuy()
+change = yourChange()
+displayOutput()
