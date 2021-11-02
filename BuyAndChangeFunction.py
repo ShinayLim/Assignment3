2f64e484dd671279f3220cf6b7e880695e61893f
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-02 01:46:30 -0700

    first commit

---
+++ BuyAndChangeFunction.py
@@ -0,0 +1,5 @@
+name = str (input("Good day! Please enter your nickname: "))
+# this will greet the person's name
+def greet(name):
+    print ("Good day "+ str(name), "! Please enjoy your shopping.")
+greet(name)
