5f5fe16238e76422a8588736ca95a26e03c17a78
SHIN_LIM <shinilim@iskolarngbayan.pup.edu.ph>
2021-11-12 22:14:04 +0800

    eigth commit

--- NameAgeAddFunction.py
+++ NameAgeAddFunction.py
@@ -1,27 +1,17 @@
-name = str (input("Welcome to our shop, what is your nickname? "))
-# this will greet the person's name
-def greet(name):
-    print("Hello, "+name+". Good Morning!")
-greet(name)
-
-# this will ask the person if the system is allowed to have their personal details
-def askYesNoQuestion(question):
-  YesNoAnswer = input(question).upper()
-  if YesNoAnswer == "YES" or YesNoAnswer == "NO":
-     return YesNoAnswer  
-  else:
-     return askYesNoQuestion(question)
+def greet():
+  name = str (input("Welcome to our shop, what is your nickname? "))
+  print("Hello, "+name+". Good Morning!")
 
 import sys
-
-answer = askYesNoQuestion("Are we allowed to gather personal information from you? (Yes or No) ")
-if answer == "YES":
-  print("We highly appreciate that. We may now proceed.")
-elif answer == "NO":
-    print("We understand your privacy concerns.Thank you.")
-    sys.exit()
-    
-#gathering of the personal details...
+def askYesNoQuestion():
+    answer = input("Are we allowed to gather personal information from you? ").upper()
+    if answer == "YES":
+        print("We highly appreciate that. We may now proceed. ")
+    elif answer == "NO":
+        print("We understand your privacy concerns.Thank you.")
+        sys.exit()
+    return answer
+        
 def getName():
     nameFunc = input("Enter your full name: ")
     return nameFunc
@@ -38,10 +28,9 @@ def displayOutput(nameF, ageF, addF):
   print ("Please check if your information below is right...")
   print (f"Hi, my name is {nameF}. I am {ageF} years old and I live in {addF}.")
 
+greet()
+askYesNoQuestion()
 name = getName()
-
 age = getAge()
-
 address = getAddress()
-
 displayOutput(name, age, address)
